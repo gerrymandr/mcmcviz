9ec39487932295f8fcdbbfc43bd7217ec6f9f531
Quinn Winters <qwinters@palantir.com>
2017-11-04 12:45:20 -0400

    last js removal

--- src/actions/ActionType.js
+++
@@ -1,4 +0,0 @@
-// ActionType.js
-
-export const ADD_COUNTER = 'ADD_COUNTER';
-export const REMOVE_COUNTER = 'REMOVE_COUNTER';
--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import counterReducer from './counterReducer';
-
-const counterApp = combineReducers({
-  counterReducer
-})
-
-export default counterApp
