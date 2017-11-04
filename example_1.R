7b607802632794a1e85f1328c8d9894a11bf9af9
Joseph Miller <jmiller@boundlessgeo.com>
2017-11-04 16:54:07 -0400

    getting styled polygons into leaflet

--- example_1.R
+++ example_1.R
@@ -4,13 +4,29 @@ library(purrr)
 library(dplyr)
 library(ggplot2)
 library(redist)
+library(leaflet)
+library(rgdal)
 
-
-nc <- st_read("data/AnneArundelN.shp")
+aa <- readOGR("data/AnneArundelN.shp",
+                  layer = "AnneArundelN", GDAL1_integer64_policy = TRUE)
+ncwgs84 <- st_read("data/AnneArundelN.shp")
+binpal <- colorBin("Blues", ncwgs84$Population, 6, pretty = FALSE)
+binpal
+m <- leaflet(ncwgs84) %>%
+  addTiles() %>%  # Add default OpenStreetMap map tiles
+  #addFeatures(st_transform(nc$geom,4326), layerId = nc$geom$id, fillColor = ~binpal(Population))
+  addPolygons(color = "#444444", weight = 1, smoothFactor = 0.5,
+              opacity = 1.0, fillOpacity = 0.5,
+              fillColor = ~binpal,
+              highlightOptions = highlightOptions(color = "white", weight = 2,
+                                                  bringToFront = TRUE))
+m
 adj_mat2 = st_touches(nc, sparse = TRUE)
 adj_mat2
 popvect = nc$Population
 numsims = 1000
 numdists = 4
 out = redist.mcmc(adjobj=adj_mat2,popvect,numsims,ndists=8,popcons=.05)
+
+
 
