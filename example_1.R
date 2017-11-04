400c0a08587134a78ac44d68230792e4e6d28cbc
Joseph Miller <jmiller@boundlessgeo.com>
2017-11-04 15:32:47 -0400

    Switched to Anne Arundel

--- example_1.R
+++ example_1.R
@@ -5,27 +5,12 @@ library(dplyr)
 library(ggplot2)
 library(redist)
 
-fname <- system.file("/home/bgock/data/cb_2016_us_state_500k.shp", package="sf")
-fname
 
-nc <- st_read(fname)
-
-nytimes = expand.grid(x = 1:5, y = 1:5) %>%
-  as.matrix() %>% 
-  st_multipoint() %>%
-  st_sfc() %>%
-  st_cast("POINT") %>%
-  st_make_grid(n = 5,5) %>%
-  st_sf() %>% 
-  mutate(
-    id = 1:n(),
-    district = rep(1:5, rep(5,5))
-  )
-
-adj_mat = st_touches(nytimes, sparse = TRUE)
-
-popvect <- runif(25, 1.0, 10000)
-numsims = 100
+nc <- st_read("data/AnneArundelN.shp")
+adj_mat2 = st_touches(nc, sparse = TRUE)
+adj_mat2
+popvect = nc$Population
+numsims = 1000
 numdists = 4
-out = redist.mcmc(adjobj=adj_mat,popvect,numsims,ndists=4)
+out = redist.mcmc(adjobj=adj_mat2,popvect,numsims,ndists=8,popcons=.2)
 
